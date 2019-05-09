7ad99c275cfb85abde235613f3a735a29506520d
Cameron Jones <cameroncjones4@gmail.com>
2019-05-09 00:11:52 -0700

    Progress bar

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
 
-const program = require('commander')
-const exec = require('child-process-promise').exec
-const crypto = require('crypto')
-const path = require('path')
 const os = require('os')
 const fs = require('fs')
+const path = require('path')
+const util = require('util')
+const crypto = require('crypto')
+const cp = require('child_process')
+const program = require('commander')
+const exec = util.promisify(cp.exec)
+const { spawn } = cp
+const ProgressBar = require('progress')
 
 const default_n = 1000
 
@@ -15,7 +19,7 @@ program
   .option('-o, --output <file>', 'output file path')
   .option('-s, --start <n>', 'starting n', default_n)
   .option('-e, --end <n>', 'ending n', default_n)
-  .option('-v, --verbose', 'verbose')
+  .option('-c, --custom <opts>', 'additional options', '1,256,1024')
   .option('-d, --debug', 'debug mode')
 
 program.parse(process.argv)
@@ -28,35 +32,47 @@ if (!program.output) return console.log('Error: Please enter an output path.');
   const parsed = path.parse(program.input)
   const input = fs.createReadStream(program.input)
   const hash = crypto.createHash('sha256')
+  const options = [
+    '-m',
+    program.custom.split(',')[0],
+    '-r',
+    program.custom.split(',')[1],
+    '-s',
+    program.custom.split(',')[2]
+  ]
   for await (const data of input) {
     hash.update(data)
   }
   const job_id = hash.digest('hex').substring(0, 8)
   const temp_path = os.tmpdir() + '/' + job_id
-  console.log(`Running job on file: ${parsed.base} (${job_id})`)
   const probe = JSON.parse((await exec(`ffprobe -v quiet -print_format json -show_format -show_streams ${program.input}`)).stdout)
   const num_frames = probe.streams[0].nb_frames
-  console.log(`Video has ${num_frames} frames`)
+  console.log(`Running job on file: ${parsed.base} (${job_id}) [${num_frames} frames]`)
   if (!fs.existsSync(`${temp_path}_0001.png`)) {
-    console.log('Converting to frames')
+    console.log('Converting to frames...')
     await exec(`ffmpeg -i ${program.input} ${temp_path}_%04d.png`)
-  } else {
-    console.log('Video already converted')
   }
   const list = Array.from({ length: num_frames }, (v, e) => e + 1)
   for (const i of list) {
     const n = parseInt(((i - 1) / (num_frames - 1)) * (program.end - program.start) + parseInt(program.start))
     const padded = i.toString().padStart(4, '0')
-    if (fs.existsSync(`${temp_path}_processed_${padded}.png`)) {
-      console.log(`Frame ${i} already processed`)
-      continue
-    }
-    console.log(`Processing frame ${i}`)
-    await exec(`primitive -i ${temp_path}_${padded}.png -o ${temp_path}_processed_${padded}.png -n ${n}`)
+    if (fs.existsSync(`${temp_path}_processed_${padded}.png`)) continue
+    const bar = new ProgressBar(`Processing frame ${i} [:bar] :percent%`, {
+      total: n,
+      complete: '=',
+      incomplete: ' ',
+      clear: true
+    })
+    await new Promise((resolve, reject) => {
+      const job = spawn('primitive', [ '-i', `${temp_path}_${padded}.png`, '-o', `${temp_path}_processed_${padded}.png`, '-n', n, '-v', ...options ])
+      job.stdout.on('data', data => bar.tick())
+      job.on('exit', code => resolve(code))
+      job.on('error', err => reject(err))
+    })
+
   }
 
 })()
-
 
 // ffmpeg -i input.mp4 output_%04d.png
 // for i in raw/*; do primitive -i $i -o processed/$i -n 100; done
